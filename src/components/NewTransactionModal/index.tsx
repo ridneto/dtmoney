32f28676d08933a9820c0b56193d144eabca8f2b
ridneto <ridineu@hotmail.com>
2021-05-09 23:49:09 -0300

    Aplicando context e hooks controle transacao

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,10 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
 
 import closeIMg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
@@ -16,17 +16,23 @@ interface NewTransactionModalProps {
 Modal.setAppElement('#root');
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
-    const [ type, setType ] = useState('deposit');
-    const [ title, setTitle ] = useState('');
-    const [  category, setCategory ] = useState('');
-    const [ value, setValue ] = useState(0);
+    const { createTransaction } = useTransactions()
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    const [type, setType] = useState('deposit');
+    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('');
+    const [amount, setAmount] = useState(0);
+
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        const data = {title, value, category, type};
+        await createTransaction({title, amount, category, type});
 
-        api.post('/transactions', data);
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit')
+        onRequestClose();
     }
 
     return (
@@ -55,8 +61,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 <input 
                     type="number" 
                     placeholder="Valor" 
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
 
                 <TransactionTypeContainer>
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,32 @@
+import { transitions } from 'polished';
+import { useContext } from 'react';
+
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container } from "./styled";
 
 export function Summary (){
+    const { transactions } = useTransactions();
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if(transaction.type === "deposit"){
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0
+    });
+
     return ( 
         <Container>
             <div>
@@ -13,7 +35,13 @@ export function Summary (){
                     <img src={incomeImg} alt="Entradas" />
                 </header>
 
-                <strong>R$1000</strong>
+                <strong>
+                    {   new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                        }).format(summary.deposits)
+                    }
+                </strong>
             </div>
 
             <div>
@@ -21,8 +49,14 @@ export function Summary (){
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Entradas" />
                 </header>
-
-                <strong>R$100</strong>
+                
+                <strong> -
+                    {   new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                        }).format(summary.withdraws)
+                    }
+                </strong>
             </div>
 
             <div className="hightlight-background">
@@ -30,8 +64,14 @@ export function Summary (){
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-
-                <strong>R$900</strong>
+                
+                <strong>
+                    {   new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                        }).format(summary.total)
+                    }
+                </strong>
             </div>
         </Container>
     )
